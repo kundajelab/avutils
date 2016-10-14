2902da072b92fb24dbcd06a7ec3388ce15004603
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2016-10-14 10:27:58 -0700

    moved make_hdf5 script to avutils, added test

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ config = {
     'packages': ['avutils'],
     'setup_requires': [],
     'install_requires': [], #gzip, re...blah
-    'scripts': ["scripts/shuffle_corresponding_lines"],
+    'scripts': ["scripts/make_hdf5", "scripts/shuffle_corresponding_lines"],
     'name': 'avutils'
 }
 
